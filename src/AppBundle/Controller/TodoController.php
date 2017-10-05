a7181f79523213eca05111b73d88f8aa791ee0f5
suwatponchana <game_bioshock@hotmail.com>
2017-10-05 23:04:57 +0700

    gamecommit#7

--- src/AppBundle/Controller/TodoController.php
+++ src/AppBundle/Controller/TodoController.php
@@ -48,8 +48,8 @@ class TodoController extends Controller
             $name        = $form['name']->getData();
             $category    = $form['category']->getData();
             $description = $form['description']->getData();
-            $priority        = $form['priority']->getData();
-            $due_date        = $form['due_date']->getData();
+            $priority    = $form['priority']->getData();
+            $due_date    = $form['due_date']->getData();
             $now = new\DateTime('now');
 
             $todo->setName($name);
@@ -72,7 +72,7 @@ class TodoController extends Controller
         }
 
         return $this->render('todo/create.html.twig',array(
-           'form' => $form ->createView()
+           'form' => $form->createView()
         ));
     }
 //******************************************************************************************
@@ -81,8 +81,62 @@ class TodoController extends Controller
      */
     public function editAction($id,Request $request)
     {
-        return $this->render('todo/edit.html.twig');
+        $todo = $this->getDoctrine()
+                     ->getRepository('AppBundle:Todo')
+                     ->find($id);
+
+        $todo->setName($todo->getName());
+        $todo->setCategory($todo->getCategory());
+        $todo->setDescription($todo->getDescription());
+        $todo->setPriority($todo->getPriority());
+        $todo->setDueDate($todo->getDueDate());
+        $todo->setCreateDate($todo->getCreateDate());
+
+        $form = $this->createFormBuilder($todo)
+            ->add('name',TextType::class, array('attr' => array('class' => 'form-control','style' => 'margin-bottom:15px')))
+            ->add('category',TextType::class, array('attr' => array('class' => 'form-control','style' => 'margin-bottom:15px')))
+            ->add('description',TextareaType::class, array('attr' => array('class' => 'form-control','style' => 'margin-bottom:15px')))
+            ->add('priority',ChoiceType::class, array('choices' => array('Low' => 'Low', 'Normal'=>'Normal', 'High' => 'High'), 'attr' => array('class' => 'form-control','style' => 'margin-bottom:15px')))
+            ->add('due_date',DateTimeType::class, array('attr' => array('class' => 'form-control','style' => 'margin-bottom:15px')))
+            ->add('save',SubmitType::class, array('label' => 'Update Todo', 'attr' => array('class' => 'btn btn-primary','style' => 'margin-bottom:15px')))
+            ->getForm();
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            //GET DATA
+            $name        = $form['name']->getData();
+            $category    = $form['category']->getData();
+            $description = $form['description']->getData();
+            $priority    = $form['priority']->getData();
+            $due_date    = $form['due_date']->getData();
+            $now = new\DateTime('now');
+
+            $em = $this->getDoctrine()->getManager();
+            $todo = $em->getRepository('AppBundle:Todo')->find($id);
+
+            $todo->setName($name);
+            $todo->setCategory($category);
+            $todo->setDescription($description);
+            $todo->setPriority($priority);
+            $todo->setDueDate($due_date);
+            $todo->setCreateDate($now);
+
+
+            $em->flush();
+
+            $this->addFlash(
+                'notice' ,
+                'Todo Update'
+            );
+
+            return $this->redirectToRoute('todo_list');
+        }
+
+        return $this->render('todo/edit.html.twig', array(
+            'todo' => $todo,
+            'form' => $form->createView()
+        ));
     }
+
 //******************************************************************************************
     /**
      * @Route("/todo/details/{id}", name="todo_details")
@@ -90,8 +144,8 @@ class TodoController extends Controller
     public function detailsAction($id)
     {
         $todo = $this->getDoctrine()
-            ->getRepository('AppBundle:Todo')
-            ->find($id);
+                     ->getRepository('AppBundle:Todo')
+                     ->find($id);
 
         return $this->render('todo/details.html.twig', array(
             'todo' => $todo
